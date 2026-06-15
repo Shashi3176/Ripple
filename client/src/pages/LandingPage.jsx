00f0b98dcf8d0a8659370a1e3ed0d49631e02dd7
Shashi3176 <shashankssgm@gmail.com>
2026-06-15 21:37:48 +0530

    fix: resolve case-sensitive import

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -7,7 +7,7 @@ import StatsCounter from "../components/landing/StatsCounter";
 import CallToActionSection from "../components/landing/CallToActionSection";
 import Footer from "../components/landing/Footer";
 
-import "../Pages/landing.css";
+import "../pages/landing.css";
 
 import SkipToContent from "../components/landing/SkipToContent";
 import ScrollToTop from "../components/landing/ScrollToTop";
